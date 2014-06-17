6717da66c8a00f3c88dcbb1b49f57a7d638d374c
Mohamed Cherif Bouchelaghem <cherifbouchelaghem@yahoo.fr>
2014-06-17 14:43:43 +0100

    button component and demo

--- src/can.touch.js
+++ src/can.touch.js
@@ -1,15 +1,12 @@
-(function(can, undefined) 
-{
-    var touch = 
-        {
-            VERSION: '0.0.1-dev',
-            Swipe: Swipe,
-            hasTouch: function() 
-            {
+(function(can, undefined) {
+    var touch = {
+        VERSION: '0.0.1-dev',
+        Swipe: Swipe,
+        hasTouch: function() {
             return 'ontouchstart' in document.documentElement;
-            }
+        }
 
-         };
+    };
 
     can.touch = touch;
     return can;
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,6 +1,31 @@
 (function(can, undefined) {
-    return can.Component.extend({
-        tag: 't-button',
-        scope: {}
+    return can.Component.extend('Button', {
+        tag: 'ui-button',
+        template: '<button class="topcoat-button--{{type}}" {{#isdisabled}}disabled{{/isdisabled}}>{{label}}</button>',
+        scope: {
+            define: {
+                isdisabled: {
+                    type: "boolean",
+                    value: false,
+                    set: function(val) {
+                        return val ? val : false;
+                    }
+                },
+                type: {
+                    type: 'string',
+                    set: function(val) {
+                        var typeList = ['quiet', 'large', 'cta', 'large--quiet', 'large--cta'];
+                        return typeList.indexOf(val) > -1 ? val : ';'
+                    }
+                },
+                label: {
+                    type: "string",
+                    set: function(val) {
+                        return val;
+                    }
+                }
+            }
+        },
+        events: {}
     });
 })(can);
